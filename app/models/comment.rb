0cabb4cd20fecc45ed3e9e8fad05ebab6df362e9
Jose Costa <jicosta@miuandes.cl>
2024-08-30 00:22:37 -0400

    Email validator partially working

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,5 +1,4 @@
-require './email_validator'
-class Comment < ApplicationRecord
+git class Comment < ApplicationRecord
     validates :author, presence: true, validator: EmailValidator, message: "must be a valid user email"
     validates :content, presence: true
 end
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,9 +1,6 @@
-require './email_validator'
-class Post < ApplicationRecord 
-    validates :author, presence: true, validator: EmailValidator, message: "must be a valid user email"
+class Post < ApplicationRecord
+    enum published: { draft: 0, published: 1 }
+    validates_with EmailValidator, attributes: [:author]
     validates :title, presence: true, length: { maximum: 100}
     validates :content, presence: true, length: { minimum: 141}
-    validates :published, presence: true, inclusion: { in: Post.published.values } 
-end
-    #No funciono correctamente el enum de published ni el requisito de el correo
-end
+  end
