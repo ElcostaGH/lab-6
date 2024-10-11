70e54e8a39b3b451257a7509135117d67512aca9
Jose Costa <jicosta@miuandes.cl>
2024-10-11 19:50:20 -0300

    Debugged some issues with registrations and logout

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -2,3 +2,14 @@
 import "@hotwired/turbo-rails"
 import "./controllers"
 import * as bootstrap from "bootstrap"
+
+document.addEventListener("turbo:load", function () {
+    const errorExplanation = document.getElementById("error_explanation");
+    if (errorExplanation) {
+      window.scrollTo({
+        top: errorExplanation.offsetTop,
+        behavior: "smooth"
+      });
+    }
+  });
+  
